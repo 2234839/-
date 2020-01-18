3b3d07753119f43c43103ead61337c452edaad1c
崮生 <admin@shenzilong.cn>
2020-01-18 14:52:32 +0800

    :bug: 修正方法调用错误

--- js 如何获取当前加载域名？/b.js
+++ js 如何获取当前加载域名？/b.js
@@ -1,4 +1,4 @@
 /** 将这个js放到其他域名 */
 const b_script = document.querySelector("script:last-child");
 console.log(b_script, b_script.src);
-document.body.outerHTML(decodeURI(b_script.src));
+document.body.outerHTML = decodeURI(b_script.src);
